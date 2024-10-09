d02af5b36b47f3ddbe367be7444024ba70433a1d
amndaaudi <143860269+amndaaudi@users.noreply.github.com>
2024-10-09 17:22:55 +0700

    Add files via upload

---
+++ Quiz/src/quiz/JOptionePane.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package quiz;
+
+/**
+ *
+ * @author amand
+ */
+class JOptionePane {
+
+    static void showMassageDialog(Object object, String nama_Mahasiswa_Tidak_Boleh_Kosong) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
